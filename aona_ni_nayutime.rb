ff430f09bccc581674fe0562f0639d4781a958ab
karubabu <karubabu8810@gmail.com>
2015-03-09 06:00:44 +0900

    no message

--- aona_ni_nayutime.rb
+++ aona_ni_nayutime.rb
@@ -7,7 +7,7 @@ Plugin.create(:aona_ni_nayutime) do
 		Service.primary.post(:message => "#{"@" + to_name + " 青菜に塩なう（悪い印象を与えた）"}", :replyto => mes)
 	end
 
-	on_mention do |s,ms|
+	on_mention do |se,ms|
 		ms.each do |m|
 			if m.user.idname=~ /uubot/  and m[:created] > DEFINED_TIME and !m.retweet? then
 				if true then
