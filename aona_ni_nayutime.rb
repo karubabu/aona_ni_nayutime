0a53702a53ab876e2d7574fcc0dbe67670bfd809
karubabu <karubabu8810@gmail.com>
2015-03-09 03:13:03 +0900

    no message

--- aona_ni_nayutime.rb
+++ aona_ni_nayutime.rb
@@ -5,11 +5,13 @@ Plugin.create(:aona_ni_nayutime) do
 		msg="@" + m.user.idname +  "青菜に塩なう（悪い印象を与えた）"
 		service.post(:message => msg, :replyto => m)
 	end
-	on_appear do |ms|
+	on_mention do |ms|
 		ms.each do |m|
 			if m.user.idname=~ /bot/ then
 				if Nrand.rand(10)<=10 then
 					sey_aona(Service.primary)
+					msg="@" + m.user.idname +  "青菜に塩なう（悪い印象を与えた）"
+					service.post(:message => msg, :replyto => m)
 				end
 			end
 		end
