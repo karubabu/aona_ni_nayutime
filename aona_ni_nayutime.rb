4d456d12ad4c7882efb51e49eabca8fe25ef3292
karubabu <karubabu8810@gmail.com>
2015-03-09 03:45:46 +0900

    no message

--- aona_ni_nayutime.rb
+++ aona_ni_nayutime.rb
@@ -1,18 +1,15 @@
 Plugin.create(:aona_ni_nayutime) do
 	DEFINED_TIME = Time.new.freeze
 	#Nrand = Random.new(100)
-	def say_aona(service)
-		msg="@" + m.user.idname +  "青菜に塩なう（悪い印象を与えた）"
-		service.post(:message => msg, :replyto => m)
+	def say_aona(s)
+		msg="@" + m.user.idname +" " + "青菜に塩なう（悪い印象を与えた）"
+		s.post(:message => msg, :replyto => m)
 	end
 	on_mention do |s,ms|
 		ms.each do |m|
-			Plugin.call(:update, nil, [Message.new(:message => "test", :system => true)])
 			if m.user.idname=~ /bot/  and m[:created] > DEFINED_TIME and !m.retweet? then
 				if true then
-					#sey_aona(Service.primary)
-					#msg="#{"@" + m.user.idname +  "青菜に塩なう（悪い印象を与えた）"}"
-					Service.primary.post(:message => "#{"@" + m.user.idname +  "青菜に塩なう（悪い印象を与えた）"}", :replyto => m)
+					sey_aona(Service.primary)
 				end
 			end
 		end
