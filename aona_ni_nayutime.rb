8659112f2eaf45234e4cb1a92b16265405186c35
karubabu <karubabu8810@gmail.com>
2015-03-09 04:27:51 +0900

    no message

--- aona_ni_nayutime.rb
+++ aona_ni_nayutime.rb
@@ -10,7 +10,7 @@ Plugin.create(:aona_ni_nayutime) do
 		ms.each do |m|
 			if m.user.idname=~ /uubot/  and m[:created] > DEFINED_TIME and !m.retweet? then
 				if true then
-					say_aona(mes)
+					say_aona(m)
 				end
 			end
 		end
